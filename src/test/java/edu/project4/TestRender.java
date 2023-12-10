79c251e08bdd288ffc99a7e38308dd57161884b1
mubarakov.cail@yandex.ru <mubarakov.cail@yandex.ru>
2023-12-10 23:06:34 +0300

    build2

--- src/test/java/edu/project4/TestRender.java
+++ src/test/java/edu/project4/TestRender.java
@@ -2,6 +2,7 @@ package edu.project4;
 
 import edu.project4.objects.Image;
 import edu.project4.objects.Pixel;
+import edu.project4.renderers.MultiThreadRenderer;
 import edu.project4.renderers.Renderer;
 import edu.project4.renderers.SingleThreadRenderer;
 import edu.project4.transformations.types.SinTransformation;
@@ -45,7 +46,7 @@ public class TestRender {
     void testMultiThreadRender() {
         Image image = Image.create(WIDTH, HEIGHT);
         List<Variation> variations = TransformationList.getVariations(new SinTransformation());
-        Renderer renderer = new SingleThreadRenderer();
+        Renderer renderer = new MultiThreadRenderer();
         renderer.render(image, variations, 1, SAMPLES, (short) ITER_PER_SAMPLES);
         for (int x = 0; x < image.width(); ++x) {
             for (int y = 0; y < image.height(); ++y) {
